2cd3f4da8e081d4e02d53cdb3fb11941369492a9
azeez12021 <esonboyev03@bk.ru>
2022-08-01 17:36:55 +0500

    fix

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Hello world</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "IndexPage",
+});
+</script>
